28b5118b9a880d49eacfc3483a2fe3bbd623b104
Pacien Boisson <pakokrew@gmail.com>
2018-10-05 09:31:03 +0200

    readme

---
+++ README.sh
@@ -0,0 +1,10 @@
+# Media downloader
+
+Based on [youtube-dl](https://github.com/rg3/youtube-dl)
+
+## Usage
+
+`./run.sh TYPE URL`
+
+Type = audio/video
+URL = any video/song/playlist on youtube, soundcloud ...
